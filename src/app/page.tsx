76d250d1e0f359fbfe2d53778176e36e05168520
David Silva <davidavilasilva@gmail.com>
2024-06-05 14:04:48 -0700

    List products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
+import { cookieBasedClient } from "@/utils/amplify-utils";
+
+export default async function Home() {
+  const { data: products, errors } =
+    await cookieBasedClient.models.Product.list();
+
+  console.log("products", products);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello, world!</h1>
+      {products?.map((product) => (
+        <div key={product.id}>{product.name}</div>
+      ))}
     </main>
   );
 }
