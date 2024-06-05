227fda75cbc42d923891b828fef5c7a1e06be359
David Silva <davidavilasilva@gmail.com>
2024-06-04 18:40:53 -0700

    Add a function to get admin status

--- src/utils/amplify-utils.ts
+++ src/utils/amplify-utils.ts
@@ -1,7 +1,7 @@
 import { createServerRunner } from "@aws-amplify/adapter-nextjs";
 import outputs from "@/../amplify_outputs.json";
 import { cookies } from "next/headers";
-import { getCurrentUser } from "aws-amplify/auth/server";
+import { getCurrentUser, fetchAuthSession } from "aws-amplify/auth/server";
 
 export const { runWithAmplifyServerContext } = createServerRunner({
   config: outputs,
@@ -14,6 +14,28 @@ export const isAuthenticated = async (): Promise<boolean> => {
       try {
         const user = await getCurrentUser(contextSpec);
         return !!user;
+      } catch (error) {
+        return false;
+      }
+    },
+  });
+};
+
+export const isAdmin = async (): Promise<boolean> => {
+  return await runWithAmplifyServerContext({
+    nextServerContext: { cookies },
+    async operation(contextSpec) {
+      let isAdmin = false;
+      try {
+        const session = await fetchAuthSession(contextSpec);
+        const tokens = session.tokens;
+        if (tokens && Object.keys(tokens).length > 0) {
+          const groups = tokens.accessToken.payload["cognito:groups"];
+          if (Array.isArray(groups) && groups.includes("Admins")) {
+            isAdmin = true;
+          }
+        }
+        return isAdmin;
       } catch (error) {
         return false;
       }
