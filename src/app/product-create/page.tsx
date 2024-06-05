35e3c5a415d3c147e72ab778ca16abee00c6bd66
David Silva <davidavilasilva@gmail.com>
2024-06-05 14:05:46 -0700

    Add ProductCreate page and component

---
+++ src/app/product-create/page.tsx
@@ -0,0 +1,6 @@
+import ProductCreate from "@/components/ProductCreate";
+
+const ProductCreatePage = () => {
+  return <ProductCreate />;
+};
+export default ProductCreatePage;
---
+++ src/components/ProductCreate.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import ProductCreateForm from "@/ui-components/ProductCreateForm";
+
+const ProductCreate = () => {
+  return <ProductCreateForm />;
+};
+export default ProductCreate;
