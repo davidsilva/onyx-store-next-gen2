3b37e3e3d92bf22a4bb0dd1e1f7696936ba18e6c
David Silva <davidavilasilva@gmail.com>
2024-06-28 12:46:24 -0700

    If somehow an unauthorized user gets to the product page...

--- src/app/admin/product-create/page.tsx
+++ src/app/admin/product-create/page.tsx
@@ -1,6 +1,12 @@
 import ProductCreate from "@/components/ProductCreate";
+import { isAdmin } from "@/utils/amplify-utils";
 
-const ProductCreatePage = () => {
+const ProductCreatePage = async () => {
+  const useIsAdmin = await isAdmin();
+  if (!useIsAdmin) {
+    return <div>Unauthorized</div>;
+  }
+
   return <ProductCreate />;
 };
 export default ProductCreatePage;
