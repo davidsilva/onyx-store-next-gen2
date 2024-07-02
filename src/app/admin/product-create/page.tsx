9b86fede6d6a2c3dcab09d98ebba96f1a564d922
David Silva <davidavilasilva@gmail.com>
2024-07-02 11:15:56 -0700

    Rely on middleware to block non-admins

--- src/app/admin/product-create/page.tsx
+++ src/app/admin/product-create/page.tsx
@@ -1,12 +1,6 @@
 import ProductCreate from "@/components/ProductCreate";
-import { isAdmin } from "@/utils/amplify-utils";
 
-const ProductCreatePage = async () => {
-  const useIsAdmin = await isAdmin();
-  if (!useIsAdmin) {
-    return <div>Unauthorized</div>;
-  }
-
+const ProductCreatePage = () => {
   return <ProductCreate />;
 };
 export default ProductCreatePage;
