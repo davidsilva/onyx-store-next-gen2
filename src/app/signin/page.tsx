a30be34fef4a599208de1bc6910285cc5ab31397
David Silva <davidavilasilva@gmail.com>
2024-07-03 16:30:29 -0700

    Add some styling

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,6 +1,19 @@
 import AuthClient from "../../components/AuthenticatorClient";
+import { checkIsAuthenticated } from "../../utils/amplify-utils";
 
-const SignIn = () => {
+const SignIn = async () => {
+  const isSignedIn = await checkIsAuthenticated();
+
+  if (isSignedIn) {
+    return (
+      <div className="flex mt-10">
+        <div className="w-1/2 border border-black rounded p-20 mx-auto text-xl">
+          You are signed in.
+        </div>
+      </div>
+    );
+  }
+
   return <AuthClient />;
 };
 
