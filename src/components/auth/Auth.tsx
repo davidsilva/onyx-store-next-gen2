e47e48c29f25554fbdd872a6cd85631e0c9aa7fa
David Silva <davidavilasilva@gmail.com>
2024-07-02 18:51:44 -0700

    Wrap children in an AdminContext

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -4,10 +4,15 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/../amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { Authenticator } from "@aws-amplify/ui-react";
+import { AdminContextProvider } from "@/context/AdminContext";
 
 Amplify.configure(outputs, { ssr: true });
 
 const Auth = ({ children }: { children: React.ReactNode }) => {
-  return <Authenticator.Provider>{children}</Authenticator.Provider>;
+  return (
+    <Authenticator.Provider>
+      <AdminContextProvider>{children}</AdminContextProvider>
+    </Authenticator.Provider>
+  );
 };
 export default Auth;
