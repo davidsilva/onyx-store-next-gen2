5d0ad640100bdddf84512fd427fdb40519ea2e64
David Silva <davidavilasilva@gmail.com>
2024-07-02 18:52:41 -0700

    Use the AdminContext to show/not show admin controls

--- src/components/ProductItemControls.tsx
+++ src/components/ProductItemControls.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { useAuthenticator } from "@aws-amplify/ui-react";
-import { fetchAuthSession } from "aws-amplify/auth";
-import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { generateClient } from "aws-amplify/api";
 import { type Schema } from "@/../amplify/data/resource";
 import clearCachesByServerAction from "@/actions/revalidate";
+import { useAdminContext } from "@/context/AdminContext";
 
 interface ProductItemControlsProps {
   id: string;
@@ -15,10 +13,7 @@ interface ProductItemControlsProps {
 const client = generateClient<Schema>();
 
 const ProductItemControls = ({ id }: ProductItemControlsProps) => {
-  const { authStatus } = useAuthenticator((context) => [context.authStatus]);
-
-  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
-  const [isAdmin, setIsAdmin] = useState<boolean>(false);
+  const { isAdmin } = useAdminContext();
 
   const router = useRouter();
 
@@ -39,55 +34,21 @@ const ProductItemControls = ({ id }: ProductItemControlsProps) => {
     }
   };
 
-  useEffect(() => {
-    if (authStatus !== "configuring") {
-      setIsSignedIn(authStatus === "authenticated");
-    }
-  }, [authStatus]);
-
-  useEffect(() => {
-    const checkAdmin = async () => {
-      let isAdmin = false;
-      try {
-        const session = await fetchAuthSession();
-        const tokens = session.tokens;
-        if (tokens && Object.keys(tokens).length > 0) {
-          const groups = tokens.accessToken.payload["cognito:groups"];
-          if (Array.isArray(groups) && groups.includes("Admins")) {
-            isAdmin = true;
-          }
-        }
-      } catch (error) {
-        isAdmin = false;
-      } finally {
-        setIsAdmin(isAdmin);
-      }
-    };
-
-    if (isSignedIn) {
-      checkAdmin();
-    } else {
-      setIsAdmin(false);
-    }
-  }, [isSignedIn]);
-
-  if (isSignedIn) {
-    return (
-      <div className="flex gap-2">
-        <button className="btn btn-blue">Add Review</button>
-        {isAdmin && (
-          <>
-            <button className="btn btn-blue" onClick={handleEdit}>
-              Edit
-            </button>
-            <button className="btn btn-blue" onClick={handleDelete}>
-              Delete
-            </button>
-          </>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className="flex gap-2">
+      <button className="btn btn-blue">Add Review</button>
+      {isAdmin && (
+        <>
+          <button className="btn btn-blue" onClick={handleEdit}>
+            Edit
+          </button>
+          <button className="btn btn-blue" onClick={handleDelete}>
+            Delete
+          </button>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default ProductItemControls;
