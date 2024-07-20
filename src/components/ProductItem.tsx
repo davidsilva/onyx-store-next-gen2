5208832cb5b760ce00754c1346b8fd51dde51d2d
David Silva <davidavilasilva@gmail.com>
2024-07-20 14:56:19 -0700

    Show first image and price

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -2,23 +2,42 @@ import { type Schema } from "@/../amplify/data/resource";
 import ProductItemControls from "@/components/ProductItemControls";
 import ImageComponent from "@/components/Image";
 
-type Product = Schema["Product"]["type"];
+type Image = {
+  key: string;
+  alt?: string | null;
+};
+
+type Product = {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  images: Image[];
+};
+
 interface ProductItemProps {
   product: Product;
   isSignedIn: boolean;
 }
 
 const ProductItem: React.FC<ProductItemProps> = ({ product, isSignedIn }) => {
+  console.log("ProductItem", product);
   return (
     <div className="rounded-lg border-black border my-1 p-2 flex gap-2">
-      {product.image && (
+      {product.images.length > 0 && (
         <div>
-          <ImageComponent path={product.image} altText={product.name} />
+          <ImageComponent path={product.images[0].key} altText={product.name} />
         </div>
       )}
       <div>
         <h2 className="text-xl font-bold">{product.name}</h2>
-        <div>{product.description}</div>
+        <p>{product.description}</p>
+        <p>
+          {product.price.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          })}
+        </p>
         <ProductItemControls id={product.id} isSignedIn={isSignedIn} />
       </div>
     </div>
