ac69bf472847a5add884c3a58d0b844fac2c2696
David Silva <davidavilasilva@gmail.com>
2024-07-26 14:44:38 -0700

    Remove ui-components files

--- src/ui-components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ProductCreateForm } from "./ProductCreateForm";
-export { default as ProductUpdateForm } from "./ProductUpdateForm";
-export { default as ImageCreateForm } from "./ImageCreateForm";
-export { default as ImageUpdateForm } from "./ImageUpdateForm";
