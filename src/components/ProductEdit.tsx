66d04d632db331f3a00c33993df38c519b97ed55
David Silva <davidavilasilva@gmail.com>
2024-08-20 15:49:39 -0700

    Refactor to use shared ProductForm component

--- src/components/ProductEdit.tsx
+++ src/components/ProductEdit.tsx
@@ -1,21 +1,12 @@
 "use client";
 
-import { useForm } from "react-hook-form";
 import { generateClient } from "@aws-amplify/api";
-import {
-  Card,
-  Alert,
-  Text,
-  TextField,
-  TextAreaField,
-  Button,
-  SelectField,
-} from "@aws-amplify/ui-react";
+import { Card, Alert, Text } from "@aws-amplify/ui-react";
 import { useState, useEffect } from "react";
 import { type Schema } from "@/../amplify/data/resource";
-import ImageUploader from "./ImageUploader";
 import clearCachesByServerAction from "@/actions/revalidate";
 import { Product, Image } from "@/types";
+import ProductForm from "./ProductForm";
 
 type FormData = {
   name: string;
@@ -37,9 +28,6 @@ interface ProductUpdateProps {
   id: string;
 }
 
-const convertPriceToDollarsAndCentsString = (price: number) =>
-  (price / 100).toFixed(2).toString();
-
 const convertPriceToCentsInteger = (price: string) => {
   const parsedPrice = parseFloat(price);
   if (isNaN(parsedPrice)) {
@@ -52,22 +40,6 @@ const ProductUpdate = ({ id }: ProductUpdateProps) => {
   const [images, setImages] = useState<Image[]>([]);
   const [message, setMessage] = useState<Message | null>(null);
   const [product, setProduct] = useState<Product | null>(null);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-    getValues,
-    setValue,
-  } = useForm<FormData>({
-    defaultValues: {
-      name: "",
-      description: "",
-      price: "",
-      mainImageS3Key: "",
-    },
-  });
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -93,12 +65,6 @@ const ProductUpdate = ({ id }: ProductUpdateProps) => {
             setImages(result.data.images);
             console.log("result.data.images", result.data.images);
           }
-          reset({
-            name: result.data.name,
-            description: result.data.description,
-            price: convertPriceToDollarsAndCentsString(result.data.price),
-            mainImageS3Key: result.data.mainImageS3Key || "",
-          });
         }
       } catch (error) {
         console.error("Could not fetch product", error);
@@ -110,20 +76,9 @@ const ProductUpdate = ({ id }: ProductUpdateProps) => {
     };
 
     fetchProduct();
-  }, [id, reset]);
+  }, [id]);
 
-  useEffect(() => {
-    // If the image selected as main image is removed, reset the main image selection
-    if (
-      images.length > 0 &&
-      !images.some((image) => image.s3Key === getValues("mainImageS3Key"))
-    ) {
-      console.log("resetting main image");
-      setValue("mainImageS3Key", "");
-    }
-  }, [images]);
-
-  const onSubmit = handleSubmit(async (data) => {
+  const onSubmit = async (data: FormData) => {
     // Only called if form validation passes, i.e., formState.isValid is true
     // Among possible improvements: check if the form data is different from the original data before submitting the update. We could maybe use the isDirty property from the formState object.
     console.log("form data", data);
@@ -179,12 +134,12 @@ const ProductUpdate = ({ id }: ProductUpdateProps) => {
         content: "An error occurred when trying to save changes.",
       });
     }
-  });
+  };
 
   if (!product) {
     return (
       <Card>
-        <Text>No products...</Text>
+        <Text>No product...</Text>
       </Card>
     );
   }
@@ -195,76 +150,13 @@ const ProductUpdate = ({ id }: ProductUpdateProps) => {
         <Alert variation={message.type}>{message.content}</Alert>
       ) : (
         <>
-          <h1 className="text-4xl font-bold text-gray-800 mb-4">
-            Update Product
-          </h1>
-          <form onSubmit={onSubmit}>
-            <div>
-              <TextField
-                label="Name"
-                hasError={!!errors.name}
-                errorMessage={errors.name?.message}
-                {...register("name", { required: "A name is required." })}
-              />
-            </div>
-            <div>
-              <TextAreaField
-                label="Description"
-                hasError={!!errors.description}
-                errorMessage={errors.description?.message}
-                {...register("description", {
-                  required: "A description is required.",
-                })}
-              />
-            </div>
-            <div>
-              <TextField
-                type="text"
-                label="Price"
-                hasError={!!errors.price}
-                errorMessage={errors.price?.message}
-                {...register("price", {
-                  required: "A price is required.",
-                  pattern: {
-                    value: /^\d+(\.\d{0,2})?$/,
-                    message:
-                      "Price must be a number with up to two decimal places.",
-                  },
-                })}
-              />
-            </div>
-            <div>
-              <Text fontWeight={600}>Images</Text>
-              <div>
-                <SelectField
-                  label="Main Image"
-                  {...register("mainImageS3Key")}
-                  onChange={(e) => {
-                    const mainImageS3Key = e.currentTarget.value;
-                    setProduct((prevProduct) => {
-                      if (prevProduct) {
-                        return {
-                          ...prevProduct,
-                          mainImageS3Key,
-                        };
-                      }
-                      return prevProduct;
-                    });
-                  }}
-                >
-                  {images.map((image, idx) => (
-                    <option key={idx} value={image.s3Key ?? ""}>
-                      {image.s3Key ?? "No S3 Key"}
-                    </option>
-                  ))}
-                </SelectField>
-              </div>
-              <ImageUploader setImages={setImages} images={images} />
-            </div>
-            <div>
-              <Button type="submit">Update Product</Button>
-            </div>
-          </form>
+          <ProductForm
+            product={product}
+            setProduct={setProduct}
+            images={images}
+            setImages={setImages}
+            onSubmit={onSubmit}
+          />
         </>
       )}
     </Card>
