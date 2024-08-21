d6b945a0f4fda73fa2e2c971f1893dc550e5a0e4
David Silva <davidavilasilva@gmail.com>
2024-08-21 12:02:40 -0700

    Use pick, omit, etc., to derive the type definitions from the schema

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,29 @@
+import { type Schema } from "@/../amplify/data/resource";
+
 export type Nullable<T> = T | null;
 
-export type Product = {
-  id: string;
-  name: string;
-  description: string;
-  price: number;
+type ProductFromSchema = Schema["Product"]["type"];
+
+// Use id, name, description, price, isArchived, and isActive from the schema.
+// images is derived from the schema using the definition below.
+// This seems like a good way limiting the definition to what we need in the code and not including what Amplify adds.
+export type Product = Omit<
+  Pick<
+    ProductFromSchema,
+    "id" | "name" | "description" | "price" | "isArchived" | "isActive"
+  >,
+  "images"
+> & {
   images: Image[];
   mainImageS3Key: Nullable<string>;
-  isArchived: boolean | null;
 };
 
-export type Image = {
-  id?: string;
+type ImageFromSchema = Schema["ProductImage"]["type"];
+
+// id, alt, and productId are optional
+// s3Key is required and can be Nullable<string>
+export type Image = Partial<
+  Pick<ImageFromSchema, "id" | "s3Key" | "alt" | "productId">
+> & {
   s3Key: Nullable<string>;
-  alt?: string | null;
-  productId?: Nullable<string>;
 };
