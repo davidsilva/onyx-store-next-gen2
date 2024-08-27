80f6dd375bab8a6601704473c0c9cf58d1fb808a
David Silva <davidavilasilva@gmail.com>
2024-08-27 16:59:59 -0700

    Create a user profile record when a user confirms sign-up

---
+++ amplify/auth/post-confirmation/handler.ts
@@ -0,0 +1,60 @@
+import type { PostConfirmationTriggerHandler } from "aws-lambda";
+import { type Schema } from "../../data/resource";
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/data";
+import { env } from "$amplify/env/post-confirmation";
+import { createUserProfile } from "./graphql/mutations";
+
+Amplify.configure(
+  {
+    API: {
+      GraphQL: {
+        endpoint: env.AMPLIFY_DATA_GRAPHQL_ENDPOINT,
+        region: env.AWS_REGION,
+        defaultAuthMode: "iam",
+      },
+    },
+  },
+  {
+    Auth: {
+      credentialsProvider: {
+        getCredentialsAndIdentityId: async () => ({
+          credentials: {
+            accessKeyId: env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+            sessionToken: env.AWS_SESSION_TOKEN,
+          },
+        }),
+        clearCredentialsAndIdentityId: () => {
+          /* noop */
+        },
+      },
+    },
+  }
+);
+
+const client = generateClient<Schema>({
+  authMode: "iam",
+});
+
+export const handler: PostConfirmationTriggerHandler = async (event) => {
+  // console.log("PostConfirmationTriggerHandler event", event);
+  await client.graphql({
+    query: createUserProfile,
+    variables: {
+      input: {
+        userId: event.userName,
+        username: event.userName,
+        preferredUsername: event.request.userAttributes.preferred_username,
+        email: event.request.userAttributes.email,
+        profileOwner: `${event.request.userAttributes.sub}::${event.userName}`,
+        givenName: event.request.userAttributes.given_name,
+        middleName: event.request.userAttributes.middle_name,
+        familyName: event.request.userAttributes.family_name,
+        birthdate: event.request.userAttributes.birthdate,
+      },
+    },
+  });
+
+  return event;
+};
