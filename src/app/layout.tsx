884e7b39f13833481906e70a78e64ed9e3d52ca4
David Silva <davidavilasilva@gmail.com>
2024-08-28 09:25:10 -0700

    Add some bottom margin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ClientSetupContextProvider>
-          <NavBar className="p-2 bg-gray-800 text-white sticky top-0 z-50" />
+          <NavBar className="p-2 bg-gray-800 text-white sticky top-0 z-50 mb-12" />
           {children}
         </ClientSetupContextProvider>
       </body>
