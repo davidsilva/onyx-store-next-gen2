873d9b3f8b4a636c34d95c596e6c8805d1fe5b7c
David Silva <davidavilasilva@gmail.com>
2024-08-28 09:27:58 -0700

    Display the preferred user name if available. Later we can use a custom hook to fetch the user name.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,19 +2,28 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { Button, Divider, Flex } from "@aws-amplify/ui-react";
+import { Button, Flex } from "@aws-amplify/ui-react";
 import { signOut } from "aws-amplify/auth";
 import { useRouter } from "next/navigation";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import { useAdminContext } from "@/context/AdminContext";
+import { type Schema } from "@/../amplify/data/resource";
+import { generateClient } from "aws-amplify/api";
 
 interface NavBarProps {
   className?: string;
 }
 
+const client = generateClient<Schema>();
+
 export default function NavBar({ className }: NavBarProps) {
-  const { authStatus } = useAuthenticator((context) => [context.authStatus]);
+  const { authStatus, user } = useAuthenticator((context) => [
+    context.authStatus,
+    context.user,
+  ]);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [userDisplayName, setUserDisplayName] = useState<string | null>(null);
+
   const { isAdmin } = useAdminContext();
 
   useEffect(() => {
@@ -22,6 +31,25 @@ export default function NavBar({ className }: NavBarProps) {
       setIsAuthenticated(authStatus === "authenticated");
     }
   }, [authStatus]);
+
+  useEffect(() => {
+    const fetchUserDisplayName = async () => {
+      const result = await client.models.UserProfile.listUserProfileByUsername({
+        username: user.username,
+      });
+      setUserDisplayName(
+        result.data[0].preferredUsername ||
+          result.data[0].email ||
+          user.username
+      );
+    };
+
+    if (authStatus === "authenticated" && user?.username) {
+      fetchUserDisplayName();
+    } else {
+      setUserDisplayName(null);
+    }
+  }, [authStatus, user]);
 
   const router = useRouter();
 
@@ -64,14 +92,17 @@ export default function NavBar({ className }: NavBarProps) {
             </Link>
           ))}
         </Flex>
-        <Button
-          variation="primary"
-          borderRadius="2rem"
-          className="mr-4"
-          onClick={signOutSignIn}
-        >
-          {isAuthenticated ? "Sign Out" : "Sign In"}
-        </Button>
+        <Flex gap="1rem" alignItems="center">
+          {userDisplayName && <Flex>{userDisplayName}</Flex>}
+          <Button
+            variation="primary"
+            borderRadius="2rem"
+            className="mr-4"
+            onClick={signOutSignIn}
+          >
+            {isAuthenticated ? "Sign Out" : "Sign In"}
+          </Button>
+        </Flex>
       </Flex>
     </div>
   );
