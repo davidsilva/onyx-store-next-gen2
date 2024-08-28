f724df51169059829c47f397a7793c6e798e0496
David Silva <davidavilasilva@gmail.com>
2024-08-28 09:26:37 -0700

    Customize AuthenticatorClient with user attributes

--- src/components/AuthenticatorClient.tsx
+++ src/components/AuthenticatorClient.tsx
@@ -1,9 +1,58 @@
 "use client";
 
 import { Authenticator } from "@aws-amplify/ui-react";
+import { Text } from "@aws-amplify/ui-react";
+
+const formFields = {
+  signUp: {
+    preferred_username: {
+      label: "Username (required)",
+      placeholder: "Choose a username",
+      isRequired: true,
+      order: 1,
+    },
+    given_name: {
+      label: "First Name",
+      placeholder: "Enter your first name",
+      isRequired: false,
+      order: 2,
+    },
+    middle_name: {
+      label: "Middle Name",
+      placeholder: "Enter your middle name",
+      isRequired: false,
+      order: 3,
+    },
+    family_name: {
+      label: "Last Name",
+      placeholder: "Enter your last name",
+      isRequired: false,
+      order: 4,
+    },
+    birthdate: {
+      label: "Birthdate",
+      placeholder: "Enter your birthdate",
+      isRequired: false,
+      order: 5,
+    },
+  },
+};
+
+const components = {
+  SignUp: {
+    Header() {
+      return (
+        <Text textAlign="center">
+          Only a username is required. Choose anything you want. We just use it
+          in the UI to make it look friendlier.
+        </Text>
+      );
+    },
+  },
+};
 
 const AuthClient = () => {
-  return <Authenticator />;
+  return <Authenticator formFields={formFields} components={components} />;
 };
 
 export default AuthClient;
