82e9e424e00a36a6c0b0816cea9ac820b45187dc
David Silva <davidavilasilva@gmail.com>
2024-08-28 09:25:50 -0700

    Begin user profile page

---
+++ src/app/user/page.tsx
@@ -0,0 +1,11 @@
+import UserProfile from "@/components/UserProfile";
+
+const UserProfilePage = () => {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
+};
+
+export default UserProfilePage;
