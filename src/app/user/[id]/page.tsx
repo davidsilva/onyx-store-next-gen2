ffafcd55f62005feef6596ea28800b3f339f0d23
David Silva <davidavilasilva@gmail.com>
2024-09-02 13:54:27 -0700

    Create a user profile page

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,9 +1,42 @@
-import UserProfile from "@/components/UserProfile";
+import { getUserProfile, UserProfileData } from "@/actions/getUserProfile";
 
-const UserProfilePage = ({ params }: { params: { id: string } }) => {
+interface UserProfilePageProps {
+  params: { id: string };
+}
+
+const UserProfilePage = async ({ params }: UserProfilePageProps) => {
+  const userProfileData = await getUserProfile(params.id);
+  console.log("userProfileData", userProfileData);
+  if (!userProfileData) {
+    return <p>User not found</p>;
+  }
   return (
     <div>
-      <UserProfile id={params.id} />
+      <h1>User Profile</h1>
+      <p>
+        <strong>Username:</strong> {userProfileData.preferredUsername}
+      </p>
+      <p>
+        <strong>Email:</strong> {userProfileData.email}
+      </p>
+      {/* preferredUsername: string; email: string; givenName: string;
+      middleName?: string; familyName: string; birthdate: string; */}
+      <p>
+        <strong>First Name: </strong>
+        {userProfileData.givenName}
+      </p>
+      <p>
+        <strong>Middle Name: </strong>
+        {userProfileData.middleName}
+      </p>
+      <p>
+        <strong>Last Name: </strong>
+        {userProfileData.familyName}
+      </p>
+      <p>
+        <strong>Birthdate: </strong>
+        {userProfileData.birthdate}
+      </p>
     </div>
   );
 };
