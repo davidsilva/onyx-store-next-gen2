bd3ceca670235a5a72af4cd0b03355f9c380d3ac
David Silva <davidavilasilva@gmail.com>
2024-09-04 17:14:12 -0700

    Add a ReviewItem component

---
+++ src/components/ReviewItem.tsx
@@ -0,0 +1,57 @@
+import { ReviewItem as ReviewItemType } from "@/types";
+import Link from "next/link";
+
+type ReviewItemProps = {
+  review: ReviewItemType;
+  currentUser: { userId: string } | null;
+};
+
+const ReviewItem = ({ review, currentUser }: ReviewItemProps) => {
+  console.log("ReviewItem review", review);
+  console.log("ReviewItem currentUser", currentUser);
+  const renderStars = (rating: number) => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <span
+          key={i}
+          className={i <= rating ? "text-yellow-500" : "text-gray-300"}
+        >
+          ★
+        </span>
+      );
+    }
+    return stars;
+  };
+
+  const formattedDate = new Date(review.createdAt).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <div className="border rounded-md border-gray-800 p-6 my-6">
+      {review.productName && (
+        <p>
+          Product:{" "}
+          <strong>
+            <Link href={`/products/${review.productId}`}>
+              {review.productName}
+            </Link>
+          </strong>
+        </p>
+      )}
+      <h3 className="text-lg font-bold">
+        {renderStars(review.rating)} {review.title}
+      </h3>
+      {review.preferredUsername && <p>By: {review.preferredUsername}</p>}
+      <p className="text-lg">{review.content}</p>
+      <p>{formattedDate}</p>
+      {currentUser && currentUser.userId === review.userId && (
+        <button className="btn btn-blue">Edit</button>
+      )}
+    </div>
+  );
+};
+export default ReviewItem;
