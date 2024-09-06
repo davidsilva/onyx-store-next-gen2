8581cf76a5042d7c21b67d8daba56f3748b9a20f
David Silva <davidavilasilva@gmail.com>
2024-09-06 12:09:25 -0700

    Add an asynchronous state type for asynchronous processes

--- src/types.ts
+++ src/types.ts
@@ -55,3 +55,40 @@ export type Message = {
   type: "error" | "success";
   content: string;
 };
+
+export enum AsyncProcessStatus {
+  NONE = "NONE",
+  PENDING = "PENDING",
+  ERROR = "ERROR",
+  SUCCESS = "SUCCESS",
+}
+
+export interface AsyncProcessBase {
+  status: AsyncProcessStatus;
+}
+
+export interface AsyncProcessNone extends AsyncProcessBase {
+  status: AsyncProcessStatus.NONE;
+}
+
+export interface AsyncProcessPending<T> extends AsyncProcessBase {
+  status: AsyncProcessStatus.PENDING;
+  value?: T;
+}
+
+export interface AsyncProcessError<T, E> extends AsyncProcessBase {
+  status: AsyncProcessStatus.ERROR;
+  error: E;
+  value?: T;
+}
+
+export interface AsyncProcessSuccess<T> extends AsyncProcessBase {
+  status: AsyncProcessStatus.SUCCESS;
+  value?: T;
+}
+
+export type AsyncProcess<T, E> =
+  | AsyncProcessNone
+  | AsyncProcessPending<T>
+  | AsyncProcessError<T, E>
+  | AsyncProcessSuccess<T>;
