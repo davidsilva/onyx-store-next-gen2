85202ba4bc22ed045eede78db3253825181b58f1
David Silva <davidavilasilva@gmail.com>
2024-09-10 15:03:03 -0700

    Add a head to the page

--- src/app/reviews/[reviewId]/edit/page.tsx
+++ src/app/reviews/[reviewId]/edit/page.tsx
@@ -2,9 +2,10 @@ import ReviewEdit from "@/components/ReviewEdit";
 
 const ReviewEditPage = ({ params }: { params: { reviewId: string } }) => {
   return (
-    <div>
+    <>
+      <h1 className="text-3xl font-bold text-gray-800">Edit Your Review</h1>
       <ReviewEdit reviewId={params.reviewId} />
-    </div>
+    </>
   );
 };
 export default ReviewEditPage;
